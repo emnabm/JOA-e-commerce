3db21af3a77ba6857301e4784bcabb32d704ac02
onszayani <ons.zayani003@gmail.com>
2026-04-28 17:15:14 +0100

    version 4

--- backend/migrations/Version20260428155856.php
+++ backend/migrations/Version20260428155856.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260425191758 extends AbstractMigration
+final class Version20260428155856 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,6 +23,11 @@ final class Version20260425191758 extends AbstractMigration
         $this->addSql('CREATE TABLE product_view (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, product_id INT DEFAULT NULL, viewed_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7315892FA76ED395 (user_id), INDEX IDX_7315892F4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE product_view ADD CONSTRAINT FK_7315892FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE product_view ADD CONSTRAINT FK_7315892F4584665A FOREIGN KEY (product_id) REFERENCES products (id)');
+        $this->addSql('ALTER TABLE message ADD last_name VARCHAR(255) NOT NULL, ADD email VARCHAR(255) NOT NULL, ADD is_read TINYINT(1) NOT NULL, CHANGE sender first_name VARCHAR(255) NOT NULL');
+        $this->addSql('DROP INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
     }
 
     public function down(Schema $schema): void
@@ -31,5 +36,10 @@ final class Version20260425191758 extends AbstractMigration
         $this->addSql('ALTER TABLE product_view DROP FOREIGN KEY FK_7315892FA76ED395');
         $this->addSql('ALTER TABLE product_view DROP FOREIGN KEY FK_7315892F4584665A');
         $this->addSql('DROP TABLE product_view');
+        $this->addSql('ALTER TABLE message ADD sender VARCHAR(255) NOT NULL, DROP first_name, DROP last_name, DROP email, DROP is_read');
+        $this->addSql('DROP INDEX IDX_75EA56E0FB7336F0 ON messenger_messages');
+        $this->addSql('DROP INDEX IDX_75EA56E0E3BD61CE ON messenger_messages');
+        $this->addSql('DROP INDEX IDX_75EA56E016BA31DB ON messenger_messages');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (queue_name, available_at, delivered_at, id)');
     }
 }
